24c110f350c3da518dac9632dcdfd504cc2ec278
asyncProgrammer <asyncProgramming@gmail.com>
2025-02-03 09:36:44 -0500

    Update websites.js

--- scripts/websites.js
+++ scripts/websites.js
@@ -17,17 +17,17 @@ websiteContainerConstructor.createShowCaseContainer('Happy Dogs','../images/dogW
 //     'A website designed to promote a small occult business.','', websites);
 
 
-websiteContainerConstructor.createShowCaseContainer('Blood Coin','../images/bloodCoinWebsiteIMG.jpg',
+websiteContainerConstructor2.createShowCaseContainer('Blood Coin','../images/bloodCoinWebsiteIMG.jpg',
     'A mock website for making plasma donations for crypto coins. - (React)','https://wizardsprogramming.github.io/bloodCoin/', websites2);
 
-websiteContainerConstructor.createShowCaseContainer('Landscaping Co.','../images/landscapingWebsiteIMG.jpg',
+websiteContainerConstructor2.createShowCaseContainer('Landscaping Co.','../images/landscapingWebsiteIMG.jpg',
     'A mock website designed for a small landscaping business (could be expanded upon for more functionality.) - (React)','https://wizardsprogramming.github.io/landscaping-mock/', websites2);
 
 
 // websiteContainerConstructor.createShowCaseContainer('Our House Kids Books','../images/ourHousewebsiteIMG.jpg',
 //     'A website designed to promote a company that writes kids books about dogs. (JS, HTML, CSS)','https:/alexisgood100.github.io/refactored-octo-waddle/', websites);
     
-    websiteContainerConstructor.createShowCaseContainer('Monster Creator','../images/monsterCreatorSiteIMG.jpg',
+    websiteContainerConstructor2.createShowCaseContainer('Monster Creator','../images/monsterCreatorSiteIMG.jpg',
         'A website designed to promote a company that sells custom monsters with removed backgrounds for games and stories. (React) ','https://wizardsprogramming.github.io/monster-creator/', websites2);
 
 

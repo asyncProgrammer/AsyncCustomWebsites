6be50e384639a18eba2099fdab1bac15d6f65ea8
asyncProgrammer <asyncProgramming@gmail.com>
2025-02-03 09:31:12 -0500

    Update websites.js

--- scripts/websites.js
+++ scripts/websites.js
@@ -1,5 +1,7 @@
 const websites = [];
 const websiteContainerConstructor = new ShowCaseConstructor();
+const websites2 = [];
+const websiteContainerConstructor2 = new ShowCaseConstructor();
 
 websiteContainerConstructor.createShowCaseContainer('Logo & Background Creation','../images/wizardsWebsiteLogo.jpg',
     'A website designed to showcase custom logos & mobile/desktop backgrounds. (JS, HTML, CSS)','https://wizardsprogramming.github.io/custom-web-sites/', websites);
@@ -16,18 +18,18 @@ websiteContainerConstructor.createShowCaseContainer('Happy Dogs','../images/dogW
 
 
 websiteContainerConstructor.createShowCaseContainer('Blood Coin','../images/bloodCoinWebsiteIMG.jpg',
-    'A mock website for making plasma donations for crypto coins. - (React)','https://wizardsprogramming.github.io/bloodCoin/', websites);
+    'A mock website for making plasma donations for crypto coins. - (React)','https://wizardsprogramming.github.io/bloodCoin/', websites2);
 
 websiteContainerConstructor.createShowCaseContainer('Landscaping Co.','../images/landscapingWebsiteIMG.jpg',
-    'A mock website designed for a small landscaping business (could be expanded upon for more functionality.) - (React)','https://wizardsprogramming.github.io/landscaping-mock/', websites);
+    'A mock website designed for a small landscaping business (could be expanded upon for more functionality.) - (React)','https://wizardsprogramming.github.io/landscaping-mock/', websites2);
 
 
 // websiteContainerConstructor.createShowCaseContainer('Our House Kids Books','../images/ourHousewebsiteIMG.jpg',
 //     'A website designed to promote a company that writes kids books about dogs. (JS, HTML, CSS)','https:/alexisgood100.github.io/refactored-octo-waddle/', websites);
     
     websiteContainerConstructor.createShowCaseContainer('Monster Creator','../images/monsterCreatorSiteIMG.jpg',
-        'A website designed to promote a company that sells custom monsters with removed backgrounds for games and stories. (React) ','https://wizardsprogramming.github.io/monster-creator/', websites);
+        'A website designed to promote a company that sells custom monsters with removed backgrounds for games and stories. (React) ','https://wizardsprogramming.github.io/monster-creator/', websites2);
 
 
-        websiteContainerConstructor.putAllShowCasesOnPage(websites, document.querySelector('.showcase-websites'));
+        websiteContainerConstructor.putAllShowCasesOnPage2(websites, document.querySelector('.showcase-websites-2'));
 console.log(websites);
